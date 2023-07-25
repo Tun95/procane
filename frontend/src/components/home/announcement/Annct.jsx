a701a16f6be7e8cdb36da76a5bca2a3079deee13
Ola Josh <akandetunji3@gmail.com>
2023-07-25 11:42:38 +0100

    new

--- frontend/src/components/home/announcement/Annct.jsx
+++ frontend/src/components/home/announcement/Annct.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./styles.css";
+import "./styles.scss";
 import show from "../../../assets/show/show.png";
 import show1 from "../../../assets/show/show1.jpg";
 
@@ -9,9 +9,16 @@ function Annct() {
       <section className="annocument background">
         <div className="container d_flex display_grid">
           <div className="img img-1">
+            <span className="explore l_flex">
+              <h2>Want More!</h2>
+            </span>
             <img src={show} width="100%" height="100%" alt="" />
           </div>
           <div className="img img-2">
+            <span className="explore l_flex">
+              <h2>Our Show Room</h2>
+              <p>Explore the vastness of our store</p>
+            </span>
             <img src={show1} width="100%" height="100%" alt="" />
           </div>
         </div>
