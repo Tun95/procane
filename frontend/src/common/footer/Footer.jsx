e919f973e2f05fcf0719e46b961e42ae2d4956ec
Ola Josh <akandetunji3@gmail.com>
2023-08-19 03:22:38 +0100

    new

--- frontend/src/common/footer/Footer.jsx
+++ frontend/src/common/footer/Footer.jsx
@@ -32,98 +32,102 @@ function Footer() {
   return (
     <>
       <footer>
-        <div className="container grid2 footer-wrap">
-          <div className="box">
-            <h1>{webname}</h1>
-            <p>{shortDesc}</p>
-            <div className="">
-              <span className="icon d_flex">
-                <Fade cascade direction="down" triggerOnce damping={0.4}>
-                  <a
-                    href={`${playstore}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="img a_flex"
-                  >
-                    <i className="fa-brands fa-google-play"></i>
-                    <span>Google Play</span>
-                  </a>
-                  <a
-                    href={`${appstore}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="img a_flex app-store"
-                  >
-                    <i className="fa-brands fa-app-store-ios"></i>
-                    <span>App Store</span>
-                  </a>
-                </Fade>
-              </span>
+        <div className="container ">
+          <div className="grid2 footer-wrap">
+            <div className="box">
+              <h1>{webname}</h1>
+              <p>{shortDesc}</p>
+              <div className="">
+                <span className="icon d_flex">
+                  <Fade cascade direction="down" triggerOnce damping={0.4}>
+                    <a
+                      href={`${playstore}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="img a_flex"
+                    >
+                      <i className="fa-brands fa-google-play"></i>
+                      <span>Google Play</span>
+                    </a>
+                    <a
+                      href={`${appstore}`}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="img a_flex app-store"
+                    >
+                      <i className="fa-brands fa-app-store-ios"></i>
+                      <span>App Store</span>
+                    </a>
+                  </Fade>
+                </span>
+              </div>
+              <hr className="horizontal" />
             </div>
-            <hr className="horizontal"/>
-          </div>
 
-          <div className="box">
-            <h2>About Us</h2>
-            <ul>
-              <Fade cascade direction="down" triggerOnce damping={0.4}>
-                <li>
-                  <Link to="/careers"> Careers</Link>
-                </li>
-                <li>
-                  <Link to="/store-locations">Our Stores</Link>
-                </li>
-                <li>
-                  <Link to="/our-cares">Our Cares</Link>
-                </li>
-                <li>
-                  <Link to="/terms-and-conditons">Terms & Conditions</Link>
-                </li>
-                <li>
-                  <Link to="/privacy-policy">Privacy Policy</Link>
-                </li>
-              </Fade>
-            </ul>
-          </div>
-          <div className="box">
-            <h2>Customer Care</h2>
-            <ul>
-              <Fade cascade direction="down" triggerOnce damping={0.4}>
-                <li>
-                  <Link to="/contact">Help Center </Link>
-                </li>
-                <li>
-                  <Link to="/how-to-buy">How to Buy </Link>
-                </li>
-                <li>
-                  <Link to="/track-order">Track Your Order </Link>
-                </li>
-                <li>
-                  <Link to="/bulk-purchases">Corporate & Bulk Purchasing </Link>
-                </li>
-                <li>
-                  <Link to="/returns">Returns & Refunds </Link>
-                </li>
-              </Fade>
-            </ul>
-          </div>
-          <div className="box">
-            <h2>Contact Us</h2>
-            <ul>
-              <Fade direction="down" triggerOnce damping={0.4}>
-                <li>{storeAddress}</li>
-              </Fade>
-              <div>
+            <div className="box float">
+              <h2>About Us</h2>
+              <ul>
                 <Fade cascade direction="down" triggerOnce damping={0.4}>
                   <li>
-                    Email: <a href={`mailto:${email}`}>{email}</a>
+                    <Link to="/careers"> Careers</Link>
                   </li>
                   <li>
-                    Phone: <a href={`tel:${whatsapp}`}>{whatsapp}</a>
-                  </li>{" "}
+                    <Link to="/store-locations">Our Stores</Link>
+                  </li>
+                  <li>
+                    <Link to="/our-cares">Our Cares</Link>
+                  </li>
+                  <li>
+                    <Link to="/terms-and-conditons">Terms & Conditions</Link>
+                  </li>
+                  <li>
+                    <Link to="/privacy-policy">Privacy Policy</Link>
+                  </li>
                 </Fade>
-              </div>{" "}
-            </ul>
+              </ul>
+            </div>
+            <div className="box float">
+              <h2>Customer Care</h2>
+              <ul>
+                <Fade cascade direction="down" triggerOnce damping={0.4}>
+                  <li>
+                    <Link to="/contact">Help Center </Link>
+                  </li>
+                  <li>
+                    <Link to="/how-to-buy">How to Buy </Link>
+                  </li>
+                  <li>
+                    <Link to="/track-order">Track Your Order </Link>
+                  </li>
+                  <li>
+                    <Link to="/bulk-purchases">
+                      Corporate & Bulk Purchasing{" "}
+                    </Link>
+                  </li>
+                  <li>
+                    <Link to="/returns">Returns & Refunds </Link>
+                  </li>
+                </Fade>
+              </ul>
+            </div>
+            <div className="box float">
+              <h2>Contact Us</h2>
+              <ul>
+                <Fade direction="down" triggerOnce damping={0.4}>
+                  <li>{storeAddress}</li>
+                </Fade>
+                <div>
+                  <Fade cascade direction="down" triggerOnce damping={0.4}>
+                    <li>
+                      Email: <a href={`mailto:${email}`}>{email}</a>
+                    </li>
+                    <li>
+                      Phone: <a href={`tel:${whatsapp}`}>{whatsapp}</a>
+                    </li>{" "}
+                  </Fade>
+                </div>{" "}
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
