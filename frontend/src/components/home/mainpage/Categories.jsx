698d13c37c921eeb57f56dce2024377d16654ee9
Ola Josh <akandetunji3@gmail.com>
2023-08-22 20:39:32 +0100

    new

--- frontend/src/components/home/mainpage/Categories.jsx
+++ frontend/src/components/home/mainpage/Categories.jsx
@@ -1,9 +1,12 @@
 import React, { useContext } from "react";
 import { useTranslation } from "react-i18next";
 import { Context } from "../../../context/Context";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./styles.scss";
 
 function Categories() {
+  const navigate = useNavigate();
+
   const { t } = useTranslation(); // Initialize the translation hook
   const { state } = useContext(Context);
   const { categories } = state;
@@ -13,10 +16,15 @@ function Categories() {
       <div className="category category-filter">
         {categories?.map((c, index) => (
           <div className="box" key={index}>
-            <Link to={`/store?category=${c.category}`} className="a_flex">
-              <img src={c.categoryImg} alt="" />
-              <span>{t(c.category)}</span>
-            </Link>
+            <a href="#store">
+              <button
+                onClick={() => navigate(`/store?category=${c.category}`)}
+                className="a_flex view_all"
+              >
+                <img src={c.categoryImg} alt="" />
+                <span>{t(c.category)}</span>
+              </button>
+            </a>
           </div>
         ))}
       </div>
