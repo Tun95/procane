b13669a043608a194e54ed77468e9b913bac4b7c
Ola Josh <akandetunji3@gmail.com>
2023-08-28 22:47:32 +0100

    new

--- backend/routes/emailMsgRoutes.js
+++ backend/routes/emailMsgRoutes.js
@@ -8,7 +8,7 @@ const sendEmailRouter = express.Router();
 
 // Contact Admin
 sendEmailRouter.post(
-  '/',
+  "/",
   expressAsyncHandler(async (req, res) => {
     const { email, name, subject, message } = req.body;
     // Create a nodemailer transport
@@ -43,41 +43,6 @@ sendEmailRouter.post(
     });
   })
 );
-
-//Contact Admin
-// sendEmailRouter.post(
-//   "/",
-//   expressAsyncHandler(async (req, res) => {
-//     const client = Sib.ApiClient.instance;
-//     const apiKey = client.authentications["api-key"];
-//     apiKey.apiKey = process.env.SEND_IN_BLUE_API_KEY;
-
-//     const { email, name, subject, message } = req.body;
-//     const tranEmailApi = new Sib.TransactionalEmailsApi();
-//     const sender = {
-//       email,
-//       name,
-//     };
-//     const receivers = [
-//       {
-//         email: process.env.EMAIL_ADDRESS,
-//       },
-//     ];
-//     tranEmailApi
-//       .sendTransacEmail({
-//         sender,
-//         to: receivers,
-//         subject,
-//         textContent: message,
-//         params: {
-//           role: "Frontend",
-//         },
-//       })
-//       .then(console.log)
-//       .catch(console.log);
-//     res.json("sent");
-//   })
-// );
 
 //Subscribe to News Letter
 sendEmailRouter.post(
